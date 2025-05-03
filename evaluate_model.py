b437f0e5ad91fc3c08c1e3dfcca135e8ae18b025
Robin Kokot <robin.edu.hr@gmail.com>
2025-05-03 23:35:57 +0200

    model trainer script

---
+++ evaluate_model.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import pickle
+import numpy as np
+from sklearn.dummy import DummyClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.model_selection import cross_val_score
+
+"""
+Main training script for our classifier models..
+
+Args:
+- features, x_train content features, and y_train labels
+
+
+"""
+
+def train_models(features):
+  return model 
+
+
+def evaluate_models(model, test):
+  return results
+
+
+def t_test(results):
+  return t_results
+
---
+++ extract_features.py
@@ -0,0 +1,14 @@
+import re
+import os
+import pandas as pd
+import numpy as np
+
+"""
+Feature extraction module which runs the preprocessing steps on our html data, and returns several different types of features for training classifiers.
+
+Args:
+  - file paths
+
+
+"""
+
