1acbc5454b1a80c7bbf47e08dc6114a042250a30
Robin Kokot <robin.edu.hr@gmail.com>
2025-05-04 23:55:55 +0200

    plan processing

--- process_html.py
+++ process_html.py
@@ -30,10 +30,44 @@ def clean_html(html):
 
   for script in soup(['script', 'style']):
     script.decompose()
+  
+  text = soup.get_text()
+
+  lines = (line.strip() for line in text.splitlines())
+  chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+  text = ' '.join(chunk for chunk in chunks if chunk)     # beautiful loop
   return text
 
-def extract_heading(html):
-  return string 
+def extract_title(html):
+  "get first heading from source"
+  soup = BeautifulSoup(html, 'html.parser')
+
+  h1 = soup.find('h1')
+  
+  if h1:
+    return h1.text.strip()
+
+  title = soup.find('title')
+  if title:
+    return title.text.strip()
+  
+
+  return ""
+
+def extract_headings(html):
+
+def extract_links(html):
+
+def extract_objects(html):
+
+def extract_keywords(html):
+
+
+def process_html_advanced(html, filen=""):
+
+  clean_text = clean_html(html)
+
+
 
 def load_datasets(dir):
   
