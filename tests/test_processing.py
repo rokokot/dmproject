7a7e15b5c15d9449fb6a3c1ee018eb54d8966c00
Robin Kokot <robin.edu.hr@gmail.com>
2025-05-05 11:07:19 +0200

    draft test

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -4,10 +4,24 @@ import pandas as pd
 
 train_data, train_labels = load_datasets('./data/train')
 
+print("running html processing tests:")
 
-example = train_data.iloc[0]
+print(f"loaded {len(train_data)} training examples")
 
+if len(train_data) > 0:
+  
+  example = train_data.iloc[0]    # take the first example
+  print('first data example contains the following features:')
+  print(f'{example['filename']}')
+  print(f'title: {example['title']}')
+  print(f'text len: {example['text_length']}')
+  print(f'headings: {example['total_headings']}')
+  print()
 
 feature_df = extract_html_features(train_data)
+print(f'features df dimensions: {feature_df.shape}')
+print(f'columns: {list(feature_df.columns)}')
 
 test_data = load_datasets('./data/test')
+print(f'loaded {len(test_data)} examples')
+
